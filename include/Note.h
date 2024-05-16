f5999122664067d742ab1cfd1513e3589eeedbbf
Kamil Fudala <kamil.fudala44@gmail.com>
2024-05-16 16:40:34 +0200

    Create getters, setters and constructor for Note.

--- include/Note.h
+++ include/Note.h
@@ -1,17 +1,40 @@
 #ifndef NOTE_H
 #define NOTE_H
 
+#include <stdexcept>
+
 #include "Duration.h"
 #include "Pitch.h"
 
-constexpr int MIN_OCTAVE = 0;
-constexpr int MAX_OCTAVE = 8;
+constexpr int MIN_OCTAVE{0};
+constexpr int MAX_OCTAVE{8};
 
 class Note
 {
     Pitch pitch;
     int octave;
     Duration duration;
+
+public:
+    [[nodiscard]] Note(const Pitch pitch, const int octave, const Duration duration)
+        : pitch(pitch),
+          octave(octave),
+          duration(duration)
+    {
+        if (octave < MIN_OCTAVE || octave > MAX_OCTAVE)
+        {
+            throw std::out_of_range("Octave out of valid range");
+        }
+    }
+
+    [[nodiscard]] auto getPitch() const -> Pitch;
+    auto setPitch(Pitch pitch) -> void;
+
+    [[nodiscard]] auto getOctave() const -> int;
+    auto setOctave(int octave) -> void;
+
+    [[nodiscard]] auto getDuration() const -> Duration;
+    auto setDuration(Duration duration) -> void;
 };
 
 
--- src/Note.cpp
+++ src/Note.cpp
@@ -1,2 +1,35 @@
 #include "../include/Note.h"
 
+auto Note::getPitch() const -> Pitch
+{
+    return pitch;
+}
+
+auto Note::setPitch(const Pitch pitch) -> void
+{
+    this->pitch = pitch;
+}
+
+auto Note::getOctave() const -> int
+{
+    return octave;
+}
+
+auto Note::setOctave(const int octave) -> void
+{
+    if (octave < MIN_OCTAVE || octave > MAX_OCTAVE)
+    {
+        throw std::out_of_range("Octave out of valid range");
+    }
+    this->octave = octave;
+}
+
+auto Note::getDuration() const -> Duration
+{
+    return duration;
+}
+
+auto Note::setDuration(const Duration duration) -> void
+{
+    this->duration = duration;
+}
