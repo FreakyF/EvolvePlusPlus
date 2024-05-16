7e466daa378847ec4e1651e938994b1aeb94faa2
Kamil Fudala <kamil.fudala44@gmail.com>
2024-05-16 17:19:14 +0200

    Create getters, setters and constructor for Individual.

--- include/Individual.h
+++ include/Individual.h
@@ -8,4 +8,18 @@ class Individual
 {
     std::vector<Note> melody;
     double fitness;
+
+public:
+    [[nodiscard]] Individual(const std::vector<Note>& melody, const double fitness)
+        : melody(melody),
+          fitness(fitness)
+    {
+    }
+
+    [[nodiscard]] auto getMelody() const -> const std::vector<Note>&;
+
+    auto setMelody(const std::vector<Note>& melody) -> void;
+
+    [[nodiscard]] double getFitness() const;
+    auto setFitness(double fitness) -> void;
 };
--- src/Individual.cpp
+++ src/Individual.cpp
@@ -1,1 +1,21 @@
 #include "../include/Individual.h"
+
+auto Individual::getMelody() const -> const std::vector<Note>&
+{
+    return melody;
+}
+
+auto Individual::setMelody(const std::vector<Note>& melody) -> void
+{
+    this->melody = melody;
+}
+
+double Individual::getFitness() const
+{
+    return fitness;
+}
+
+auto Individual::setFitness(const double fitness) -> void
+{
+    this->fitness = fitness;
+}
